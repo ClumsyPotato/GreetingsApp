b762bb1f2eca190d56818c0a9d749e4789116a0e
Adrian Tomalla <adrian.tomalla@novatec-gmbh.de>
2019-03-08 11:26:25 +0100

    "added a test that is going to fail"

--- src/test/java/com/example/GreetingsApp/GreetingsAppApplicationTests.java
+++ src/test/java/com/example/GreetingsApp/GreetingsAppApplicationTests.java
@@ -5,12 +5,20 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GreetingsAppApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+
+	@Test
+	public void testThatIsGoingToFail(){
+		assertTrue("This is failing on purpose", false);
 	}
 
 }
