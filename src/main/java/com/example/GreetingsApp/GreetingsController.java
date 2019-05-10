5638ffbf0e17914e8edaa2d3ca06c6e2bbddac26
Adrian Tomalla <adrian.tomalla@novatec-gmbh.de>
2019-05-10 23:12:53 +0200

    Some functionality

--- src/main/java/com/example/GreetingsApp/GreetingsController.java
+++ src/main/java/com/example/GreetingsApp/GreetingsController.java
@@ -3,17 +3,32 @@ package com.example.GreetingsApp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class GreetingsController {
 
     @GetMapping("/hello/stranger")
     public String greetStranger(){
-        return "Greetings Stranger";
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        System.out.println("Stranger has been called:" +timeStamp);
+        return "Greetings Stranger:" + timeStamp;
     }
 
     @GetMapping("/hello/friend")
     public String helloStranger(){
-        return "Greetings Friend";
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        System.out.println("Friend  has been called:" +timeStamp);
+        return "Greetings Friend:" + timeStamp;
+    }
+
+
+    @GetMapping("/bye/friend")
+    public String byeFriend(){
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        System.out.println("Bye friend has been called:" +timeStamp);
+        return "Bye Friend:" + timeStamp;
     }
 
 }
